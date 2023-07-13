6613e2d98d07ffa60615cec6b201b0fce79842f5
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-13 18:32:37 +0530

    Completed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,71 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
+import { IconButton, Box, Typography } from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
-}
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const activeImage = images[activeIndex];
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrevious = () => {
+    setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="image-container">
+        <img
+          src={activeImage.img}
+          alt={activeImage.title}
+          className="carousel-image"
+        />
+        <div className="carousel-background">
+          <div className="caption-container">
+            <Box
+              bgcolor="rgba(255, 255, 255, 0.7)"
+              borderRadius="10px"
+              p={2}
+              className="caption-box"
+            >
+              <Typography variant="h4" component="h2" gutterBottom>
+                {activeImage.title}
+              </Typography>
+            </Box>
+            <Box
+              bgcolor="rgba(255, 255, 255, 0.7)"
+              borderRadius="10px"
+              p={2}
+              className="caption-box"
+            >
+              <Typography variant="subtitle1" component="p">
+                {activeImage.subtitle}
+              </Typography>
+            </Box>
+          </div>
+          <div className="carousel-controls">
+            <IconButton
+              onClick={handlePrevious}
+              className="carousel-control"
+            >
+              <ArrowBackIosIcon />
+            </IconButton>
+            <IconButton
+              onClick={handleNext}
+              className="carousel-control"
+            >
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
